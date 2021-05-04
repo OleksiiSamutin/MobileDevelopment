5cc5ffb384b07d80a75d0ca9bec8ea932f8ba6b0
OleksiiSamutin <samutin63>
2021-05-04 12:17:28 +0300

    Made 4 lab

--- Components/Movie/Movie.js
+++ Components/Movie/Movie.js
@@ -1,96 +1,270 @@
 import React from "react";
-import { StyleSheet, Text, View, Image, Dimensions } from "react-native";
+import { StyleSheet, Text, View, Image, Dimensions, Alert } from "react-native";
 import * as ScreenOrientation from "expo-screen-orientation";
 import Posters from "./PostersExport";
+import { TouchableHighlight } from "react-native-gesture-handler";
 const windowWidth = Dimensions.get("window").width;
+import Swipeout from "react-native-swipeout";
 
-class Movie {
-  constructor({ Title, Year, imdbID, noid, Type, Poster, Details: {
-    Rated,
-    Released,
-    Runtime,
-    Genre,
-    Director,
-    Writer,
-    Actors,
-    Plot,
-    Language,
-    Country,
-    Awards,
-    imdbRating,
-    imdbVotes,
-    Production
-  } }) {
-    this.title = Title;
-    this.year = Year;
-    this.imdbID = imdbID;
-    this.noid = noid;
-    this.type = Type;
-    this.poster = Poster;
-    this.production = Production
+class Movie extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      clicked: "NOPE",
+    };
+    // this = {...props}
+    // // { Title, Year, imdbID, noid, Type, Poster, Details: {
+    // //   Rated,
+    //   Released,
+    //   Runtime,
+    //   Genre,
+    //   Director,
+    //   Writer,
+    //   Actors,
+    //   Plot,
+    //   Language,
+    //   Country,
+    //   Awards,
+    //   imdbRating,
+    //   imdbVotes,
+    //   Production
+    // } }) {
+    //   this.title = Title;
+    //   this.year = Year;
+    //   this.imdbID = imdbID;
+    //   this.noid = noid;
+    //   this.type = Type;
+    //   this.poster = Poster;
+    //   this.production = Production
 
+    // }
   }
   findPosterImage = () => {
-    if (this.poster && Posters[this.poster.split(".jpg")[0]]) {
-      return Posters[this.poster.split(".jpg")[0]];
+    if (this.props.Poster && Posters[this.props.Poster.split(".jpg")[0]]) {
+      return Posters[this.props.Poster.split(".jpg")[0]];
     } else {
       return false;
     }
   };
-  renderMoviePreview = (windowWidth) => {
+  openDetails = () => {
+    this.setState({ clicked: "YOU CLICKED" });
+  };
+  render() {
+    let swipeBtns = [
+      {
+        text: "Delete",
+        backgroundColor: "red",
+        underlayColor: "rgba(0, 0, 0, 1, 0.6)",
+        onPress: () => {
+          this.props.deleteMovie(this.props.keyId);
+        },
+      },
+    ];
     const styles = StyleSheet.create({
-        container: {
-          flex: 1,
-          backgroundColor: "#fff",
-          alignItems: "center",
-          flexDirection: "row",
-          justifyContent: "flex-start",
-          padding: 10,
-          borderBottomColor: "lightgrey",
-          borderBottomWidth: 1,
-          width: windowWidth,
-        },
-        col1: {
-          paddingRight: 10,
-          backgroundColor: "white",
-        },
-        col2: {
-          width: windowWidth - 80,
-        },
-        images: {
-          flex: 1,
-          backgroundColor: "white",
-          width: 60,
-          height: 60,
-          padding: 10,
-          // resizeMode: 'contain'
-        },
-      });
+      container: {
+        flex: 1,
+        backgroundColor: "#fff",
+        alignItems: "center",
+        flexDirection: "row",
+        justifyContent: "flex-start",
+        padding: 10,
+        borderBottomColor: "lightgrey",
+        borderBottomWidth: 1,
+        width: this.props.screenWidth,
+      },
+      col1: {
+        paddingRight: 10,
+        backgroundColor: "white",
+      },
+      col2: {
+        width: this.props.screenWidth - 80,
+      },
+      images: {
+        flex: 1,
+        backgroundColor: "white",
+        width: 60,
+        height: 60,
+        padding: 10,
+        // resizeMode: 'contain'
+      },
+      detailsWrapper: {
+        flex: 1,
+        flexDirection: "column",
+        padding: 15,
+      },
+      detailsImages: {
+        flex: 1,
+        alignSelf: "center",
+        alignItems: "center",
+        justifyContent: "center",
+        backgroundColor: "white",
+        width: 150,
+        height: 240,
+        padding: 10,
+        marginBottom: 10,
+      },
+      details: {
+        color: "#666",
+      },
+      defaultTextColor: {
+        color: "#000",
+      },
+      detailsTextContainer: {
+        marginBottom: 15,
+      },
+    });
     return (
-      <View style={styles.container}>
-        <View style={styles.col1}>
-          {this.findPosterImage() ? (
-            <Image
-              source={this.findPosterImage()}
-              style={styles.images}
-              width="40"
-            />
-          ) : (
-            <View style={styles.images}></View>
-          )}
-        </View>
-        <View style={styles.col2}>
-          <View>
-            <Text>{this.title}</Text>
-            <Text>{this.year}</Text>
-            {/* <Text>{this.imdbID}</Text> */}
-            <Text>{this.noid}</Text>
-            <Text>{this.type}</Text>
+      <View>
+        {this.props.withDetails ? (
+          <View style={styles.detailsWrapper}>
+            {this.findPosterImage() ? (
+              <Image
+                source={this.findPosterImage()}
+                style={styles.detailsImages}
+                width="150"
+              />
+            ) : (
+              <View style={styles.images}></View>
+            )}
+            <View style={styles.detailsTextContainer}>
+              <Text style={styles.details}>
+                Title:{" "}
+                <Text style={styles.defaultTextColor}> {this.props.Title}</Text>
+              </Text>
+              <Text style={styles.details}>
+                Year:{" "}
+                <Text style={styles.defaultTextColor}>
+                  {" "}
+                  {this.props.Details.Year}
+                </Text>
+              </Text>
+              <Text style={styles.details}>
+                Genre:{" "}
+                <Text style={styles.defaultTextColor}>
+                  {" "}
+                  {this.props.Details.Genre}
+                </Text>
+              </Text>
+            </View>
 
+            <View style={styles.detailsTextContainer}>
+              <Text style={styles.details}>
+                Director:{" "}
+                <Text style={styles.defaultTextColor}>
+                  {" "}
+                  {this.props.Details.Director}
+                </Text>
+              </Text>
+              <Text style={styles.details}>
+                Actors:{" "}
+                <Text style={styles.defaultTextColor}>
+                  {" "}
+                  {this.props.Details.Actors}
+                </Text>
+              </Text>
+            </View>
+
+            <View style={styles.detailsTextContainer}>
+              <Text style={styles.details}>
+                Country:{" "}
+                <Text style={styles.defaultTextColor}>
+                  {" "}
+                  {this.props.Details.Country}
+                </Text>
+              </Text>
+              <Text style={styles.details}>
+                Language:{" "}
+                <Text style={styles.defaultTextColor}>
+                  {" "}
+                  {this.props.Details.Language}
+                </Text>
+              </Text>
+              <Text style={styles.details}>
+                Production:{" "}
+                <Text style={styles.defaultTextColor}>
+                  {" "}
+                  {this.props.Details.Production}
+                </Text>
+              </Text>
+              <Text style={styles.details}>
+                Released:{" "}
+                <Text style={styles.defaultTextColor}>
+                  {" "}
+                  {this.props.Details.Released}
+                </Text>
+              </Text>
+              <Text style={styles.details}>
+                Runtime:{" "}
+                <Text style={styles.defaultTextColor}>
+                  {" "}
+                  {this.props.Details.Runtime}
+                </Text>
+              </Text>
+            </View>
+
+            <View style={styles.detailsTextContainer}>
+              <Text style={styles.details}>
+                Awards:{" "}
+                <Text style={styles.defaultTextColor}>
+                  {" "}
+                  {this.props.Details.Awards}
+                </Text>
+              </Text>
+              <Text style={styles.details}>
+                Raiting:{" "}
+                <Text style={styles.defaultTextColor}>
+                  {" "}
+                  {this.props.Details.imdbRating}/10
+                </Text>
+              </Text>
+            </View>
+
+            <Text style={styles.details}>
+              Plot:{" "}
+              <Text style={styles.defaultTextColor}>
+                {" "}
+                {this.props.Details.Plot}
+              </Text>
+            </Text>
           </View>
-        </View>
+        ) : (
+          <Swipeout
+            right={swipeBtns}
+            autoClose="true"
+            backgroundColor="transparent"
+          >
+            <TouchableHighlight
+              onPress={() => {
+                this.props.openDetails(this.props);
+              }}
+            >
+              <View style={styles.container}>
+                <View style={styles.col1}>
+                  {this.findPosterImage() ? (
+                    <Image
+                      source={this.findPosterImage()}
+                      style={styles.images}
+                      width="40"
+                    />
+                  ) : (
+                    <View style={styles.images}></View>
+                  )}
+                </View>
+                <View style={styles.col2}>
+                  <View>
+                    <Text>{this.props.Title}</Text>
+                    <Text>{this.props.Year}</Text>
+                    {/* <Text>{this.imdbID}</Text> */}
+                    <Text>{this.props.noid}</Text>
+                    <Text>{this.props.Type}</Text>
+                  </View>
+                </View>
+              </View>
+            </TouchableHighlight>
+          </Swipeout>
+        )}
       </View>
     );
-  };
+  }
 }
 export default Movie;
--- Views/MoviesView/MoviesView.js
+++ Views/MoviesView/MoviesView.js
@@ -1,11 +1,21 @@
-import React, { Component} from "react";
-import { StyleSheet, Text, View,Dimensions } from "react-native";
+import React, { Component } from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  Dimensions,
+  TextInput,
+  Button,
+  Alert,
+  KeyboardAvoidingView
+} from "react-native";
+import { SearchBar } from "react-native-elements";
 import { ScrollView } from "react-native-gesture-handler";
 import Movie from "../../Components/Movie";
 import * as data from "./files/MoviesList.json";
 import MovieDetails from "../../Components/Movie/MovieDetailsExport";
 import * as ScreenOrientation from "expo-screen-orientation";
-
+import { AntDesign } from "@expo/vector-icons";
 const styles = StyleSheet.create({
   container: {
     flex: 1,
@@ -15,6 +25,36 @@ const styles = StyleSheet.create({
     justifyContent: "center",
     fontSize: 1,
   },
+  backButton: {
+    paddingTop: 10,
+    paddingLeft: 10,
+    marginRight: "auto",
+  },
+  addFilm: {
+    position: "absolute",
+    top: -45,
+    right: 10,
+  },
+  addNewFilmWrapper: {
+    paddingTop: 10,
+  },
+  input: {
+    borderWidth: 1,
+    width: Dimensions.get("window").width,
+    marginTop: 10,
+    marginBottom: 20,
+  },
+  inputsWrapper: {
+    marginTop: 20,
+    marginBottom: 20,
+  },
+  upperSearchPanel: {
+    backgroundColor: "#FCE762",
+  },
+  searchBar: {},
+  addFilmBtnWrapp: {
+    alignSelf: "flex-end",
+  },
 });
 
 class MoviesView extends React.Component {
@@ -22,9 +62,15 @@ class MoviesView extends React.Component {
     super(props);
     this.state = {
       orientation: "portrait",
-      screenWidth: Dimensions.get("window").width
+      screenWidth: Dimensions.get("window").width,
+      screenHeight: Dimensions.get("window").height,
+      movies: data.Search,
+      search: "",
     };
   }
+  openDetails = (filmInfo) => {
+    this.setState({ isOpenDetails: true, currentMovie: filmInfo });
+  };
   componentWillUnmount() {
     ScreenOrientation.removeOrientationChangeListeners();
   }
@@ -44,19 +90,191 @@ class MoviesView extends React.Component {
           ? "portrait"
           : "landscape";
 
-      this.setState({ orientation: orientation,screenWidth: Dimensions.get("window").width });
+      this.setState({
+        orientation: orientation,
+        screenWidth: Dimensions.get("window").width,
+        screenHeight: Dimensions.get("window").height,
+      });
     });
   }
+  deleteMovie = (id) => {
+    const copyBeforeDelete = [...this.state.movies];
+    let newArray = [];
+    if (id === 0) {
+      newArray = copyBeforeDelete.slice(1);
+    } else if (id === copyBeforeDelete.length - 1) {
+      newArray = copyBeforeDelete.slice(0, id);
+    } else {
+      newArray = [
+        ...copyBeforeDelete.slice(0, id),
+        ...copyBeforeDelete.slice(id + 1, copyBeforeDelete.length),
+      ];
+    }
+    this.setState({ movies: newArray });
+  };
+  updateSearch = (search) => {
+    this.setState({ search });
+  };
+  addFilm = () => {
+    const yearsRegex = /^[12][0-9]{3}$/;
+    if (
+      this.state.newFilmType &&
+      this.state.newFilmTitle &&
+      this.state.newFilmType
+    ) {
+      if (this.state.newFilmYear.match(yearsRegex)) {
+        const newFilm = {
+          Year: this.state.newFilmYear,
+          Type: this.state.newFilmType,
+          Title: this.state.newFilmTitle,
+        };
+        const updatedListMovies = [...this.state.movies, newFilm];
+        this.setState({
+          movies: updatedListMovies,
+          addNewFilm: false,
+          newFilmYear: "",
+          newFilmType: "",
+          newFilmTitle: "",
+        });
+      } else {
+        Alert.alert("Not valid year fild");
+      }
+    } else {
+      Alert.alert("One of the field is empty");
+    }
+  };
+
   render() {
+    const {
+      addNewFilm,
+      screenWidth,
+      screenHeight,
+      isOpenDetails,
+      currentMovie,
+      movies,
+      search,
+    } = this.state;
+    const filteredMovies = movies
+      .map((film, i) => {
+        const Details = MovieDetails[film.imdbID]
+          ? MovieDetails[film.imdbID]
+          : {};
+        return (
+          <Movie
+            {...film}
+            Details={Details}
+            screenWidth={screenWidth}
+            openDetails={this.openDetails}
+            keyId={i}
+            deleteMovie={this.deleteMovie}
+          />
+        );
+      })
+      .filter((movie) => {
+        if (movie.props.Title.toLowerCase().includes(search.toLowerCase())) {
+          return movie;
+        }
+      });
     return (
-      <View style={styles.container}>
-        <ScrollView>
-          {data.Search.map((film) => {
-            const Details = MovieDetails[film.imdbID] ? MovieDetails[film.imdbID] : {};
-            return new Movie({...film, Details}).renderMoviePreview(this.state.screenWidth);
-          })}
-        </ScrollView>
-      </View>
+      <KeyboardAvoidingView style={styles.container}>
+        {addNewFilm ? (
+          <ScrollView
+            stickyHeaderIndices={[0]}
+            style={styles.addNewFilmWrapper}
+          >
+            <AntDesign
+              name="back"
+              size={24}
+              color="black"
+              onPress={() => {
+                this.setState({ isOpenDetails: false, addNewFilm: false });
+              }}
+            />
+            <View style={styles.inputsWrapper}>
+              <Text>Title</Text>
+              <TextInput
+                style={styles.input}
+                width={screenWidth - 20}
+                onChangeText={(text) => {
+                  this.setState({ newFilmTitle: text });
+                }}
+              />
+              <Text>Type</Text>
+              <TextInput
+                style={styles.input}
+                width={screenWidth - 20}
+                onChangeText={(text) => {
+                  this.setState({ newFilmType: text });
+                }}
+              />
+              <Text>Year</Text>
+              <TextInput
+                style={styles.input}
+                width={screenWidth - 20}
+                keyboardType="numeric"
+                onChangeText={(text) => {
+                  this.setState({ newFilmYear: text });
+                }}
+              />
+              <Button title="Add" onPress={this.addFilm} />
+            </View>
+          </ScrollView>
+        ) : isOpenDetails ? (
+          <ScrollView stickyHeaderIndices={[0]}>
+            <AntDesign
+              name="back"
+              size={24}
+              color="black"
+              onPress={() => {
+                this.setState({ isOpenDetails: false });
+              }}
+              style={styles.backButton}
+            />
+            <Movie {...currentMovie} withDetails={true} />
+          </ScrollView>
+        ) : (
+          <ScrollView stickyHeaderIndices={[0]}>
+            <View style={styles.upperSearchPanel}>
+              <SearchBar
+                placeholder="Type Here..."
+                onChangeText={this.updateSearch}
+                value={search}
+                containerStyle={{
+                  ...styles.searchBar,
+                  width: screenWidth - 40,
+                }}
+                lightTheme={true}
+              />
+              <View style={styles.addFilmBtnWrapp}>
+                <AntDesign
+                  name="plus"
+                  size={24}
+                  color="black"
+                  style={styles.addFilm}
+                  onPress={() => {
+                    this.setState({ addNewFilm: true });
+                  }}
+                />
+              </View>
+            </View>
+
+            {filteredMovies.length === 0 ? (
+              <View
+                style={{
+                  width: screenWidth,
+                  height: screenHeight/2,
+                  justifyContent: "center",
+                  alignItems: "center",
+                }}
+              >
+                <Text>No items found</Text>
+              </View>
+            ) : (
+              filteredMovies
+            )}
+          </ScrollView>
+        )}
+      </KeyboardAvoidingView>
     );
   }
 }
